12b6e5cdc2d9e99b2ae1ab187770a2cd40207117
Felipe Avila Silva <fasilva@inf.ufpel.edu.br>
2019-10-04 18:49:01 -0300

    essa eh 8.c

--- teste.c
+++ teste.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    int i = 10;
+
+    int vet[10] = {5,3,7,9,1,6,0,8,672,85}, cout = 0, tmp;
+
+    printf("vetor sem ordenar:\n");
+        for(int i=0; i<10; i++){
+            printf("%d ", vet[i]);
+        }
+    printf("\n\n");
+
+        for(int i=0; i < 10; i++){
+            for(int k=0; k<10-1;k++){
+                if(vet[i] < vet[k]){
+                    tmp = vet[i];
+                    vet[i] = vet[k];
+                    vet[k] = tmp;
+                    cout ++;
+                    if(cout == 4){
+                        printf("vetor apos 4 trocas:\n");
+                        for(int j=0; j<10;j++){
+                            printf("%d ", vet[j]);
+                        }
+                    printf("\n\n");
+                    }
+                }
+            }
+        }
+    printf("vetor totalmente ordenado:\n");
+        for(int i=0; i<10;i++){
+            printf("%d ", vet[i]);
+        }   
+    printf("\n\n");
     return 0;
 }
